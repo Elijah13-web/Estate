3e11158fb3fee862720eae8a9efd21001ecf5cd5
Ajiboye Elijah <ajiboyeelijah242@gmail.com>
2026-02-17 11:26:03 -0800

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-white text-4xl font-bold font-serif">
+        Estate App Ready 🚀
+        <h2 className="text-sm font-serif text-blue-300"> Markson Ajiboye is ready to build amazing things with React and Tailwind CSS!</h2>
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
